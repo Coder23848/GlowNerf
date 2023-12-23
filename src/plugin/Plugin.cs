ed65afbf8a093b142732f6c7a4f4a688a3b9e232
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2023-12-23 14:21:00 -0500

    fading update

--- src/plugin/Plugin.cs
+++ src/plugin/Plugin.cs
@@ -1,4 +1,6 @@
-﻿using BepInEx;
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using BepInEx;
 using UnityEngine;
 
 namespace GlowNerf
@@ -14,17 +16,124 @@ namespace GlowNerf
             On.RainWorld.OnModsInit += RainWorld_OnModsInit;
 
             On.PlayerGraphics.Update += PlayerGraphics_Update;
+            On.VoidSea.VoidSeaScene.VoidSeaTreatment += VoidSeaScene_VoidSeaTreatment;
+            On.SaveState.RainCycleTick += SaveState_RainCycleTick;
+            On.OracleSwarmer.BitByPlayer += OracleSwarmer_BitByPlayer;
+            On.SLOracleSwarmer.BitByPlayer += SLOracleSwarmer_BitByPlayer;
+            On.Spear.HitSomethingWithoutStopping += Spear_HitSomethingWithoutStopping;
+            On.Player.ctor += Player_ctor;
         }
 
         private const float VANILLALIGHTRAD = 300f;
         private const float VANILLALIGHTALPHA = 1f;
+        private const string CYCLES_SINCE_NEURON_SAVE_STRING = "23848.glownerf.CYCLESSINCENEURON";
+        private readonly ConditionalWeakTable<SaveState, StrongBox<int>> cyclesSinceNeuronCache = new();
+        private int GetCyclesSinceNeuron(SaveState saveState)
+        {
+            if (cyclesSinceNeuronCache.TryGetValue(saveState, out StrongBox<int> value))
+            {
+                return value.Value;
+            }
+            else if (!saveState.unrecognizedSaveStrings.Any(x => x.StartsWith(CYCLES_SINCE_NEURON_SAVE_STRING)))
+            {
+                cyclesSinceNeuronCache.Add(saveState, new(0));
+                return 0;
+            }
+            else
+            {
+                int ret = int.Parse(saveState.unrecognizedSaveStrings.First(x => x.StartsWith(CYCLES_SINCE_NEURON_SAVE_STRING)).Split(':')[1]);
+                cyclesSinceNeuronCache.Add(saveState, new(ret));
+                return ret;
+            }
+        }
+        private void SetCyclesSinceNeuron(SaveState saveState, int cyclesSinceNeuron)
+        {
+            saveState.unrecognizedSaveStrings.RemoveAll(x => x.StartsWith(CYCLES_SINCE_NEURON_SAVE_STRING));
+            saveState.unrecognizedSaveStrings.Add(CYCLES_SINCE_NEURON_SAVE_STRING + ":" + cyclesSinceNeuron);
+            cyclesSinceNeuronCache.Remove(saveState);
+        }
+        private float GlowMultiplier(Player self)
+        {
+            if (PluginOptions.GlowFades.Value && self.room.world.game.IsStorySession)
+            {
+                float multiplier = Mathf.Lerp(1, 0, (float)GetCyclesSinceNeuron(self.abstractCreature.world.game.GetStorySession.saveState) / PluginOptions.GlowFadeTime.Value);
+                return PluginOptions.IntensityMultiplier.Value * multiplier;
+            }
+            else
+            {
+                return PluginOptions.IntensityMultiplier.Value;
+            }
+        }
+        private void ResetGlow(Player player)
+        {
+            PlayerGraphics graphics = player.graphicsModule as PlayerGraphics;
+            if (graphics.lightSource != null)
+            {
+                graphics.lightSource.HardSetRad(VANILLALIGHTRAD * GlowMultiplier(player));
+                graphics.lightSource.HardSetAlpha(VANILLALIGHTALPHA * GlowMultiplier(player));
+            }
+        }
+        private void OracleSwarmer_BitByPlayer(On.OracleSwarmer.orig_BitByPlayer orig, OracleSwarmer self, Creature.Grasp grasp, bool eu)
+        {
+            orig(self, grasp, eu);
+            if (PluginOptions.GlowFades.Value && self.room.world.game.IsStorySession && self.bites < 1 && grasp.grabber is Player p && (!ModManager.MSC || !p.isNPC))
+            {
+                SetCyclesSinceNeuron(self.room.world.game.GetStorySession.saveState, 0);
+                ResetGlow(p);
+            }
+        }
+        private void SLOracleSwarmer_BitByPlayer(On.SLOracleSwarmer.orig_BitByPlayer orig, SLOracleSwarmer self, Creature.Grasp grasp, bool eu)
+        {
+            orig(self, grasp, eu);
+            if (PluginOptions.GlowFades.Value && self.room.world.game.IsStorySession && self.bites < 1 && grasp.grabber is Player p && (!ModManager.MSC || !p.isNPC))
+            {
+                SetCyclesSinceNeuron(self.room.world.game.GetStorySession.saveState, 0);
+                ResetGlow(p);
+            }
+        }
+        private void Spear_HitSomethingWithoutStopping(On.Spear.orig_HitSomethingWithoutStopping orig, Spear self, PhysicalObject obj, BodyChunk chunk, PhysicalObject.Appendage appendage)
+        {
+            orig(self, obj, chunk, appendage);
+            if (PluginOptions.GlowFades.Value && self.room.world.game.IsStorySession && self.Spear_NeedleCanFeed() && obj is OracleSwarmer && self.thrownBy is Player p && (!ModManager.MSC || !p.isNPC))
+            {
+                SetCyclesSinceNeuron(self.room.world.game.GetStorySession.saveState, 0);
+                ResetGlow(p);
+            }
+        }
+        private void SaveState_RainCycleTick(On.SaveState.orig_RainCycleTick orig, SaveState self, RainWorldGame game, bool depleteSwarmRoom)
+        {
+            orig(self, game, depleteSwarmRoom);
+            if (PluginOptions.GlowFades.Value && self.theGlow)
+            {
+                SetCyclesSinceNeuron(self, GetCyclesSinceNeuron(self) + 1);
+            }
+        }
+        private void Player_ctor(On.Player.orig_ctor orig, Player self, AbstractCreature abstractCreature, World world)
+        {
+            orig(self, abstractCreature, world);
+            if (PluginOptions.GlowFades.Value && self.AI == null && self.glowing && abstractCreature.Room.world.game.IsStorySession && GlowMultiplier(self) <= 0)
+            {
+                self.glowing = false;
+            }
+        }
+        // Actually apply the mod
         private void PlayerGraphics_Update(On.PlayerGraphics.orig_Update orig, PlayerGraphics self)
         {
             orig(self);
             if (self.lightSource != null && self.lightSource.Rad == VANILLALIGHTRAD && self.lightSource.Alpha == VANILLALIGHTALPHA)
             {
-                self.lightSource.HardSetRad(VANILLALIGHTRAD * PluginOptions.IntensityMultiplier.Value);
-                self.lightSource.HardSetAlpha(VANILLALIGHTALPHA * PluginOptions.IntensityMultiplier.Value);
+                self.lightSource.HardSetRad(VANILLALIGHTRAD * GlowMultiplier(self.player));
+                self.lightSource.HardSetAlpha(VANILLALIGHTALPHA * GlowMultiplier(self.player));
+            }
+        }
+        // The Void Sea sets the player's light source every frame, as it slowly fades it out as you get deeper in.
+        private void VoidSeaScene_VoidSeaTreatment(On.VoidSea.VoidSeaScene.orig_VoidSeaTreatment orig, VoidSea.VoidSeaScene self, Player player, float swimSpeed)
+        {
+            orig(self, player, swimSpeed);
+            if (player.graphicsModule != null && (player.graphicsModule as PlayerGraphics).lightSource != null)
+            {
+                (player.graphicsModule as PlayerGraphics).lightSource.setAlpha *= GlowMultiplier(player);
+                (player.graphicsModule as PlayerGraphics).lightSource.setRad *= GlowMultiplier(player);
             }
         }
 
--- src/plugin/PluginOptions.cs
+++ src/plugin/PluginOptions.cs
@@ -7,7 +7,12 @@ namespace GlowNerf
     {
         public static PluginOptions Instance = new();
 
-        public static Configurable<float> IntensityMultiplier = Instance.config.Bind("IntensityMultiplier", 0.5f, new ConfigurableInfo("The intensity of the glow effect; 0 is no glow, 1 is the normal amount."));
+        public static Configurable<float> IntensityMultiplier = Instance.config.Bind("IntensityMultiplier", 0.5f, new ConfigurableInfo("The intensity of the glow effect; 0 is no light, 1 is the normal amount."));
+        //public static Configurable<float> LanternIntensityMultiplier = Instance.config.Bind("LanternIntensityMultiplier", 1f, new ConfigurableInfo("The intensity of lantern light; 0 is no light, 1 is the normal amount."));
+        public static Configurable<bool> GlowFades = Instance.config.Bind("GlowFades", false, new ConfigurableInfo("Causes the glow effect to disappear over time."));
+        //public static Configurable<bool> LanternsFade = Instance.config.Bind("LanternsFade", false, new ConfigurableInfo("Causes lanterns to lose their light over time."));
+        public static Configurable<int> GlowFadeTime = Instance.config.Bind("GlowFadeTime", 10, new ConfigurableInfo("The number of cycles it takes to lose the glow effect.", new ConfigAcceptableRange<int>(1, 1000)));
+        //public static Configurable<int> LanternFadeTime = Instance.config.Bind("LanternFadeTime", 10, new ConfigurableInfo("The number of cycles it takes for a lantern to die out.", new ConfigAcceptableRange<int>(1, 1000)));
 
         public override void Initialize()
         {
@@ -15,7 +20,19 @@ namespace GlowNerf
             Tabs = new OpTab[1];
 
             Tabs[0] = new(Instance, "Options");
-            SliderOption(IntensityMultiplier, 200, 0, "Intensity");
+            SliderOption(IntensityMultiplier, 200, 0, "Glow Intensity");
+            //SliderOption(LanternIntensityMultiplier, 200, 1, "Lantern Intensity");
+
+            Tabs[0].AddItems(
+                new OpCheckBox(GlowFades, new(50, 460)) { description = GlowFades.info.description },
+                new OpLabel(new Vector2(90, 460), new Vector2(), "Glow fades over ", FLabelAlignment.Left),
+                new OpUpdown(GlowFadeTime, new(150 + 35, 455), 50) { description = GlowFadeTime.info.description },
+                new OpLabel(new Vector2(205 + 35, 460), new Vector2(), "cycles", FLabelAlignment.Left));
+            //Tabs[0].AddItems(
+            //    new OpCheckBox(LanternsFade, new(50, 430)) { description = LanternsFade.info.description },
+            //    new OpLabel(new Vector2(90, 430), new Vector2(), "Lanterns fade over ", FLabelAlignment.Left),
+            //    new OpUpdown(LanternFadeTime, new(150 + 49, 425), 50) { description = LanternFadeTime.info.description },
+            //    new OpLabel(new Vector2(205 + 49, 430), new Vector2(), "cycles", FLabelAlignment.Left));
         }
 
         private void CheckBoxOption(Configurable<bool> setting, float pos, string label)
@@ -24,7 +41,7 @@ namespace GlowNerf
         }
         private void SliderOption(Configurable<float> setting, int size, float pos, string label)
         {
-            Tabs[0].AddItems(new OpFloatSlider(setting, new(50, 545 - pos * 30), size) { description = setting.info.description }, new OpLabel(new Vector2(60 + size, 550 - pos * 30), new Vector2(), label, FLabelAlignment.Left));
+            Tabs[0].AddItems(new OpFloatSlider(setting, new(50, 545 - pos * 30), size) { description = setting.info.description, Increment = size / 200 }, new OpLabel(new Vector2(60 + size, 550 - pos * 30), new Vector2(), label, FLabelAlignment.Left));
         }
     }
 }
